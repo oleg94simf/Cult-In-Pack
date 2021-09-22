0c28ae20a60f965c12515224b84435165170dd22
Oleh <70809643+oleg94simf@users.noreply.github.com>
2021-09-22 20:37:18 +0300

    checkboxfinal

--- js/modal.js
+++ js/modal.js
@@ -159,7 +159,7 @@ document.addEventListener('DOMContentLoaded', function () {
  *************************************/
 
 
-
+/* 
 $('.checkbox__active').change(function () {
 	$('.checkbox__buttom').prop("disabled", !this.checked);
 
@@ -180,5 +180,83 @@ $('.checkbox__active').change(function () {
 
 }).change()
 
+ */
 
 
+
+function checkbox() {
+	const buttonCheckbox = document.querySelector('.checkbox__buttom');
+	const label = document.querySelector('label');
+
+	const conteinerActivation = document.querySelector('.content');
+	const checkbox = document.querySelector('.checkbox');
+	if (buttonCheckbox) {
+		conteinerActivation.classList.remove('__none');
+		checkbox.classList.add('__none');
+	}
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+	const form = document.getElementById('checkbox');
+
+	form.addEventListener('submit', formSend);
+
+	async function formSend(e) {
+		e.preventDefault();
+
+		let error = formValidate(form);
+		if (error === 0) {
+			checkbox();
+
+			form.reset();
+
+
+		} else {
+
+
+		}
+
+
+	}
+
+
+	function formValidate(form) {
+		let error = 0;
+		let formReq = document.querySelectorAll('._req');
+
+		for (let index = 0; index < formReq.length; index++) {
+			const input = formReq[index];
+			formRemoveError(input);
+
+			if (input.getAttribute("type") === "checkbox" && input.checked === false) {
+				formAddError(input);
+				error++;
+			}
+
+			const checbox = document.querySelector('#rules');
+			checbox.onfocus = function () {
+				if (this.classList.contains('_error')) {
+					// удаляем индикатор ошибки, т.к. пользователь хочет ввести данные заново
+					this.classList.remove('_error');
+				}
+			};
+
+		}
+
+		return error;
+
+	}
+
+	function formAddError(input) {
+		input.parentElement.classList.add('_error')
+		input.classList.add('_error');
+	}
+
+	function formRemoveError(input) {
+		input.parentElement.classList.remove('_error')
+		input.classList.remove('_error');
+
+	}
+
+
+});
